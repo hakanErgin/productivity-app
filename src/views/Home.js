644958a95b014c898d19e58746a9233088118539
hakanErgin <hakan.ergin@gmail.com>
2020-06-11 17:58:46 +0200

    signin works with token

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,9 +14,22 @@ class HomeScreen extends React.Component {
       <View style={styles.container}>
         <Button title="Show me more of the app" onPress={this._showMoreApp} />
         <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+        <Button title="print token :)" onPress={this._getData} />
       </View>
     );
   }
+
+  _getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('userToken');
+      if (value !== null) {
+        // return <Text>{value}</Text>;
+        return console.log(value);
+      }
+    } catch (e) {
+      return <Text>{e}</Text>;
+    }
+  };
 
   _showMoreApp = () => {
     this.props.navigation.navigate('Other');
--- src/views/Signin.js
+++ src/views/Signin.js
@@ -1,30 +1,56 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, TextInput} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import gql from 'graphql-tag';
 import {useMutation, useQuery} from '@apollo/react-hooks';
+import {Formik} from 'formik';
 
 import styles from '../stlyes';
 
 const SIGN_IN = gql`
-  mutation {
-    Login(email: "hakan.ergin@gmail.com", password: "123")
+  mutation Login($email: String!, $password: String!) {
+    Login(email: $email, password: $password)
   }
 `;
 
 const SignInScreen = props => {
-  const [signin, {data}] = useMutation(SIGN_IN);
-
-  const _signInAsync = async () => {
-    await AsyncStorage.setItem('userToken', 'abc');
-    props.navigation.navigate('App');
-  };
+  const [signin] = useMutation(SIGN_IN, {
+    onCompleted(data) {
+      AsyncStorage.setItem('userToken', data.Login);
+      props.navigation.navigate('App');
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <Button title="Sign in!" onPress={_signInAsync} />
-      {data && console.log(data)}
-      {/* {data && <Text>{data.users[0].email}</Text>} */}
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => {
+          signin({
+            variables: {
+              email: values.email,
+              password: values.password,
+            },
+          });
+        }}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <View>
+            <TextInput
+              placeholder="Email"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            <TextInput
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
